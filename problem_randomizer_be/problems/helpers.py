f00773e134fe2e8d2a857d95eb062fa5f743441a
Nguyen Cao Son <nguyencaoson7921@gmail.com>
2023-11-24 02:10:16 +0700

    done atcoder

--- problem_randomizer_be/problems/helpers.py
+++ problem_randomizer_be/problems/helpers.py
@@ -24,7 +24,7 @@ async def get_codeforces_problems():
             contest_name=contest_dict[problem["contestId"]]["name"],
             url=f"https://codeforces.com/problemset/problem/{problem['contestId']}/{problem['index']}",
             rating=problem["rating"],
-            source_type="cf",
+            source_type=constants.CODEFORCES,
         )
         for problem in problems
         if "rating" in problem
@@ -37,3 +37,37 @@ def update_codeforces_problems():
     if problems:
         Problem.objects.filter(source_type=constants.CODEFORCES).delete()
         Problem.objects.bulk_create(problems)
+
+
+async def get_atcoder_problems():
+    urls = [
+        "https://kenkoooo.com/atcoder/resources/problems.json",
+        "https://kenkoooo.com/atcoder/resources/problem-models.json",
+        "https://kenkoooo.com/atcoder/resources/contests.json",
+    ]
+    client = httpx.AsyncClient()
+    fetches = [client.get(url) for url in urls]
+    problem_data, problem_model_data, contest_data = [data.json() for data in await asyncio.gather(*fetches)]
+    await client.aclose()
+    contest_dict = {contest["id"]: contest for contest in contest_data}
+    prob_with_models = filter(
+        lambda prob: prob["id"] in problem_model_data and "difficulty" in problem_model_data[prob["id"]], problem_data
+    )
+    problems = [
+        Problem(
+            name=problem["name"],
+            contest_name=contest_dict[problem["contest_id"]]["title"],
+            url=f"https://atcoder.jp/contests/{problem['contest_id']}/tasks/{problem['id']}",
+            rating=problem_model_data[problem["id"]]["difficulty"],
+            source_type=constants.ATCODER,
+        )
+        for problem in prob_with_models
+    ]
+    return problems
+
+
+def update_atcoder_problems():
+    problems = async_to_sync(get_atcoder_problems)()
+    if problems:
+        Problem.objects.filter(source_type=constants.ATCODER).delete()
+        print(Problem.objects.bulk_create(problems))
--- problem_randomizer_be/problems/views.py
+++ problem_randomizer_be/problems/views.py
@@ -1,19 +1,25 @@
 from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from problem_randomizer_be.problems import constants
-from problem_randomizer_be.problems.helpers import update_codeforces_problems
+from problem_randomizer_be.problems import constants, helpers
 from problem_randomizer_be.problems.models import Problem
 from problem_randomizer_be.problems.serializers import ProblemSerializer
 
 
 class UpdateProblemsViewSet(APIView):
+    UPDATE_FUNCTION_MAP = {
+        constants.CODEFORCES: helpers.update_codeforces_problems,
+        constants.ATCODER: helpers.update_atcoder_problems,
+    }
+
     def get(self, request, source_type):
-        if source_type == constants.CODEFORCES:
+        if source_type in self.UPDATE_FUNCTION_MAP:
+            update_func = self.UPDATE_FUNCTION_MAP[source_type]
             try:
-                update_codeforces_problems()
+                update_func()
                 return Response(status=status.HTTP_200_OK)
             except Exception as e:
                 return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
@@ -24,3 +30,15 @@ class UpdateProblemsViewSet(APIView):
 class ProblemViewSet(ReadOnlyModelViewSet):
     queryset = Problem.objects.all()
     serializer_class = ProblemSerializer
+
+    @action(detail=False)
+    def codeforces(self, request):
+        cf = self.queryset.filter(source_type=constants.CODEFORCES)
+        serializer = ProblemSerializer(cf, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False)
+    def atcoder(self, request):
+        ac = self.queryset.filter(source_type=constants.ATCODER)
+        serializer = ProblemSerializer(ac, many=True)
+        return Response(serializer.data)
