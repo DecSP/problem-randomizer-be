98575004278abd8675f94d094592b5d36aee2e94
Nguyen Cao Son <nguyencaoson7921@gmail.com>
2023-12-07 23:08:49 +0700

    Fix source type serializer

--- problem_randomizer_be/problems/serializers.py
+++ problem_randomizer_be/problems/serializers.py
@@ -4,6 +4,11 @@ from problem_randomizer_be.problems.models import Problem
 
 
 class ProblemSerializer(serializers.ModelSerializer[Problem]):
+    source_type = serializers.SerializerMethodField(source="get_source_type_display")
+
+    def get_source_type(self, obj):
+        return obj.get_source_type_display().lower()
+
     class Meta:
         model = Problem
         fields = ["source_type", "name", "contest_name", "url", "rating"]
