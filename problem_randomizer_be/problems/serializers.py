3d71c7e377e92f55c2622de485bf5c554e86c763
Nguyen Cao Son <nguyencaoson7921@gmail.com>
2023-12-24 14:29:12 +0700

    add id field and  content

--- problem_randomizer_be/problems/serializers.py
+++ problem_randomizer_be/problems/serializers.py
@@ -1,15 +1,24 @@
 from rest_framework import serializers
 
 from problem_randomizer_be.problems.models import Problem
+from problem_randomizer_be.problems.services.atcoder import AtcoderService
 
 
 class ProblemSerializer(serializers.ModelSerializer[Problem]):
     class Meta:
         model = Problem
-        fields = ["source_type", "name", "contest_name", "url", "rating"]
+        fields = ["id", "source_type", "name", "contest_name", "url", "rating"]
 
 
 class ProblemDetailSerializer(serializers.ModelSerializer[Problem]):
+    atcoder_service = AtcoderService()
+    content = serializers.SerializerMethodField()
+
     class Meta:
         model = Problem
-        fields = ["source_type", "name", "contest_name", "url", "rating", "content"]
+        fields = ["id", "source_type", "name", "contest_name", "url", "rating", "content"]
+
+    def get_content(self, obj):
+        if obj.source_type == Problem.SourceType.ATCODER:
+            return self.atcoder_service.get_atcoder_content(obj.url)
+        return "NOT IMPLEMENTED"
