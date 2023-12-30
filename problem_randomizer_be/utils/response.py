3c7ef676ea759f7eea6999ee0572ae983ed3a784
Nguyen Cao Son <nguyencaoson7921@gmail.com>
2023-12-30 16:18:23 +0700

    fix code for custom response

--- problem_randomizer_be/utils/response.py
+++ problem_randomizer_be/utils/response.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 
 
-class CustomResponse:
+class CustomResponse(Response):
     def __init__(self, status_code, message, data):
-        return Response(status=status_code, data={"statusCode": status_code, "message": message, "data": data})
+        super().__init__(status=status_code, data={"statusCode": status_code, "message": message, "data": data})
